a436632b016e679fc2b598b7117d4cd2fa6c6c7e
WantedWarlock <azureazulg@gmail.com>
2015-07-26 13:36:53 -0400

    Block Edits
    EditedBlocks

--- src/main/java/com/steveuniverse/blocks/ModBlocks.java
+++ src/main/java/com/steveuniverse/blocks/ModBlocks.java
@@ -1,5 +1,8 @@
 package com.steveuniverse.blocks;
 
+import com.steveuniverse.client.ModCreativeTabs;
+import com.steveuniverse.main.StevesUniverse;
+
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
@@ -10,7 +13,7 @@ public class ModBlocks {
 	garnet_block, jasper_block, jasper_ore, malachite_block, opal_block, 
 	pearl_block, pearl_ore, peridot_block, peridot_ore, rainbow_block, 
 	rose_block, rose_ore, ruby_block, sapphire_block, sapphire_ore, 
-	sardonyx_block, sugilite_block;
+	sardonyx_block, sugilite_block, ruby_ore;
 	
 	public static void mainRegistry(){
 		initBlocks();
@@ -18,26 +21,27 @@ public class ModBlocks {
 	}
 	
 	public static void initBlocks(){
-		alexandrite_block = new SUBlock(Material.iron);
-		amethyst_block = new SUBlock(Material.iron);
-		amethyst_ore = new SUBlock(Material.rock);
-		garnet_block = new SUBlock(Material.iron);
-		jasper_block = new SUBlock(Material.iron);
-		jasper_ore = new SUBlock(Material.rock);
-		malachite_block = new SUBlock(Material.iron);
-		opal_block = new SUBlock(Material.iron);
-		pearl_block = new SUBlock(Material.iron);
-		pearl_ore = new SUBlock(Material.rock);
-		peridot_block = new SUBlock(Material.iron);
-		peridot_ore = new SUBlock(Material.rock);
-		rainbow_block = new SUBlock(Material.iron);
-		rose_block = new SUBlock(Material.iron);
-		rose_ore = new SUBlock(Material.rock);
-		ruby_block = new SUBlock(Material.iron);
-		sapphire_block = new SUBlock(Material.iron);
-		sapphire_ore = new SUBlock(Material.rock);
-		sardonyx_block = new SUBlock(Material.iron);
-		sugilite_block = new SUBlock(Material.iron);
+		alexandrite_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		amethyst_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		amethyst_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		garnet_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		jasper_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		jasper_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		malachite_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		opal_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		pearl_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		pearl_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		peridot_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		peridot_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		rainbow_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		rose_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		rose_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		ruby_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		ruby_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		sapphire_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		sapphire_ore = new SUBlock(Material.rock).setCreativeTab(ModCreativeTabs.tabSU).setHardness(4.0F);
+		sardonyx_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
+		sugilite_block = new SUBlock(Material.iron).setCreativeTab(ModCreativeTabs.tabSU).setLightLevel(1.0F).setHardness(5.0F).setResistance(30F);
 	}
 	
 	public static void regBlock(){
@@ -52,6 +56,7 @@ public class ModBlocks {
 		GameRegistry.registerBlock(rainbow_block, "rainbow_block");
 		GameRegistry.registerBlock(rose_block, "rose_block");
 		GameRegistry.registerBlock(ruby_block, "ruby_block");
+		GameRegistry.registerBlock(ruby_ore, "ruby_ore");
 		GameRegistry.registerBlock(sapphire_block, "sapphire_block");
 		GameRegistry.registerBlock(sardonyx_block, "sardonyx_block");
 		GameRegistry.registerBlock(sugilite_block, "sugilite_block");
