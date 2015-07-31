a6858142cd6cc2c2250dad9692a4afa1109d5aad
Burnt Router <kbeddek@gmail.com>
2015-07-31 01:03:37 -0400

    Fixed AI

--- src/main/java/com/steveuniverse/entities/peridot_entity.java
+++ src/main/java/com/steveuniverse/entities/peridot_entity.java
@@ -5,12 +5,14 @@ import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackOnCollide;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
 import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
 import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
 import net.minecraft.entity.ai.EntityAISwimming;
 import net.minecraft.entity.ai.EntityAIWander;
 import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
@@ -19,6 +21,15 @@ public class peridot_entity extends EntityMob{
 
 	public peridot_entity(World p) {
 		super(p);
+		this.getNavigator().setAvoidsWater(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityZombie.class, 1.0D, false));
+		this.tasks.addTask(4, new EntityAIMoveThroughVillage(this, 1.0D, false));
+		this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityZombie.class, 8.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityZombie.class, 0, true));
 		
 	}
 	@Override
--- src/main/java/com/steveuniverse/entities/rose_entity.java
+++ src/main/java/com/steveuniverse/entities/rose_entity.java
@@ -2,7 +2,16 @@ package com.steveuniverse.entities;
 
 import net.minecraft.entity.EnumCreatureAttribute;
 import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackOnCollide;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAIMoveThroughVillage;
+import net.minecraft.entity.ai.EntityAINearestAttackableTarget;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.world.World;
 
 public class rose_entity extends EntityMob{
@@ -11,6 +20,15 @@ public class rose_entity extends EntityMob{
 	public rose_entity(World r) {
 		super(r);
 		setSize(4F, 4F);
+		this.getNavigator().setAvoidsWater(true);
+		this.tasks.addTask(0, new EntityAISwimming(this));
+		this.tasks.addTask(6, new EntityAIWander(this, 1.0D));
+		this.tasks.addTask(2, new EntityAIAttackOnCollide(this, EntityZombie.class, 1.0D, false));
+		this.tasks.addTask(4, new EntityAIMoveThroughVillage(this, 1.0D, false));
+		this.tasks.addTask(7, new EntityAIWatchClosest(this, EntityZombie.class, 8.0F));
+		this.tasks.addTask(7, new EntityAILookIdle(this));
+		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, true));
+		this.targetTasks.addTask(2, new EntityAINearestAttackableTarget(this, EntityZombie.class, 0, true));
 	}
 	@Override
 	protected void applyEntityAttributes()
