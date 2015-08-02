0b0107ba967deddfcc4e14856edad48d3c408f57
WantedWarlock <azureazulg@gmail.com>
2015-08-02 17:55:26 -0400

    Entity Throwable Fixes

--- src/main/java/com/steveuniverse/main/StevesUniverse.java
+++ src/main/java/com/steveuniverse/main/StevesUniverse.java
@@ -3,13 +3,26 @@ package com.steveuniverse.main;
 import com.steveuniverse.blocks.ModBlocks;
 import com.steveuniverse.client.ModCreativeTabs;
 import com.steveuniverse.entities.ModEntities;
+import com.steveuniverse.entities.lapislazuli_entity;
+import com.steveuniverse.entities.opal_entity;
+import com.steveuniverse.entities.pearl_entity;
+import com.steveuniverse.entities.pearlspear_entity;
+import com.steveuniverse.entities.peridot_entity;
+import com.steveuniverse.entities.peridotlaser_entity;
+import com.steveuniverse.entities.rose_entity;
+import com.steveuniverse.entities.ruby_entity;
+import com.steveuniverse.entities.sapphire_entity;
+import com.steveuniverse.entities.sonicscreech_entity;
 import com.steveuniverse.items.ModItems;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.EntityRegistry;
 
 @Mod(modid = StevesUniverse.MODID, name = "Steve's Universe", version = StevesUniverse.VERSION)
 
@@ -33,5 +46,22 @@ public class StevesUniverse {
 		proxy.registerRenderers();
 		
 	}
+	@EventHandler
+	public static void load(FMLInitializationEvent event) {
+	EntityRegistry.registerModEntity(peridotlaser_entity.class, "PeridotLaser", 1, instance , 64, 10, true);	
+	EntityRegistry.registerModEntity(pearlspear_entity.class, "PearlS", 2, instance , 64, 10, true);	
+	EntityRegistry.registerModEntity(sonicscreech_entity.class, "SonicScreech", 3, instance , 64, 10, true);	
+	EntityRegistry.registerModEntity(pearl_entity.class, "Pearl", 5, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(peridot_entity.class, "Peridot", 6, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(rose_entity.class, "RoseQuartz", 7, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(lapislazuli_entity.class, "LapisLazuli", 8, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(ruby_entity.class, "Ruby", 9, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(sapphire_entity.class, "Sapphire", 10, instance , 64, 10, true);
+	EntityRegistry.registerModEntity(opal_entity.class, "Opal", 11, instance , 64, 10, true);
+}
+@EventHandler
+	public static void PostLoad(FMLPostInitializationEvent Post) {
+			
+}
 
 }
