52a4a6d205a0d74cb4c98275fb2a838b1e40cc74
WantedWarlock <azureazulg@gmail.com>
2015-08-02 11:55:22 -0400

    Mod Entity

--- src/main/java/com/steveuniverse/client/ClientProxy.java
+++ src/main/java/com/steveuniverse/client/ClientProxy.java
@@ -15,6 +15,7 @@ import com.steveuniverse.items.ModItems;
 import com.steveuniverse.main.CommonProxy;
 import com.steveuniverse.models.amethyst_model;
 import com.steveuniverse.models.garnet_model;
+import com.steveuniverse.models.garnet_model_1;
 import com.steveuniverse.models.lapislazuli_model;
 import com.steveuniverse.models.pearl_model;
 import com.steveuniverse.models.peridot_model;
@@ -64,7 +65,7 @@ public class ClientProxy extends CommonProxy {
 				  BiomeGenBase.plains);
 		RenderingRegistry.registerEntityRenderingHandler(amethyst_entity.class, new amethyst_render(new amethyst_model(), 1f));
 		
-		RenderingRegistry.registerEntityRenderingHandler(garnet_entity.class, new garnet_render(new garnet_model(), 1f));
+		RenderingRegistry.registerEntityRenderingHandler(garnet_entity.class, new garnet_render(new garnet_model_1(), 1f));
 	}
 
 }
--- src/main/java/com/steveuniverse/renderers/garnet_render.java
+++ src/main/java/com/steveuniverse/renderers/garnet_render.java
@@ -1,18 +1,37 @@
 package com.steveuniverse.renderers;
 
+import org.lwjgl.opengl.GL11;
+
+import com.steveuniverse.entities.garnet_entity;
+
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.renderer.entity.RenderLiving;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
 
 public class garnet_render extends RenderLiving{
 
 	public garnet_render(ModelBase parModelBase, float parShadowSize) {
 		super(parModelBase, parShadowSize);
+	
 	}
 
+	 @Override
+	    protected void preRenderCallback(EntityLivingBase entity, float f)
+	    {
+	        preRenderCallbackGarnet((garnet_entity) entity, f);
+	    }
+	  
+	    protected void preRenderCallbackGarnet(garnet_entity entity, float f)
+	    {
+	    	GL11.glScalef(5F, 5F, 5F); 
+	    }
+	
+	
+	
 
-	public static final ResourceLocation textureLocation = new ResourceLocation("steveuniverse:textures/entity/Garnet.png");
+	public static final ResourceLocation textureLocation = new ResourceLocation("steveuniverse:textures/entity/Garnet_3.png");
 
 	@Override
 	protected ResourceLocation getEntityTexture(Entity entity) 
